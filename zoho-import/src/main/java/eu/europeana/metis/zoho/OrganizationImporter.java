73d9c6152055ace83c8e0fa44aadc0e55d14d43e
rgraf <roman.graf@ait.ac.at>
2018-04-26 16:21:52 +0200

    #EA-1072 updated synchronization of deletes and delete by loosing roles according to pull request review.

--- zoho-import/src/main/java/eu/europeana/metis/zoho/OrganizationImporter.java
+++ zoho-import/src/main/java/eu/europeana/metis/zoho/OrganizationImporter.java
@@ -13,6 +13,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -22,6 +23,7 @@ import org.slf4j.LoggerFactory;
 import eu.europeana.corelib.definitions.edm.entity.Organization;
 import eu.europeana.enrichment.api.external.model.zoho.ZohoOrganization;
 import eu.europeana.enrichment.service.EntityService;
+import eu.europeana.enrichment.service.dao.ZohoV2AccessDao;
 import eu.europeana.enrichment.service.exception.ZohoAccessException;
 import eu.europeana.enrichment.service.zoho.ZohoAccessService;
 import eu.europeana.metis.authentication.dao.ZohoAccessClientDao;
@@ -120,20 +122,106 @@ public class OrganizationImporter {
       lastRun = entityService.getLastOrganizationImportDate();
 
     List<ZohoOrganization> orgList;
+    List<String> toDelete = new ArrayList<>();
+    List<ZohoOrganization> toUpdate = new ArrayList<>();
+
     int start = 1;
     final int rows = 100;
     boolean hasNext = true;
     // Zoho doesn't return the number of organizations in get response.
     while (hasNext) {
+      // start with clear list of deletions for each Zoho page
+      toDelete.clear();
+      // start with clear list of updates for each Zoho page
+      toUpdate.clear();
+
       orgList = zohoAccessService.getOrganizations(start, rows, lastRun, searchCriteria);
-      importedOrgs += storeOrganizations(orgList);
+      fillDeletionAndUpdateLists(orgList, toUpdate, toDelete);
+
+      /* store new and update organizations changed in Zoho */
+      importedOrgs += storeOrganizations(toUpdate);
+
+      /* delete organizations that do not match filter criteria due to change role in Zoho */
+      deleteOrganizations(toDelete);
+
       start += rows;
       // if no more organizations exist in Zoho
-      if (orgList.size() < rows)
+      if (orgList.size() < rows) {
         hasNext = false;
+      }
     }
 
     LOGGER.info("Successfully imported organizations: " + importedOrgs);
+
+    int numberOfDeletedDbOrganizations = synchronizeDeletedZohoOrganizations();
+    LOGGER.info("Successfully deleted from DB organizations: " + numberOfDeletedDbOrganizations);
+  }
+
+  /**
+   * This method performs synchronization of organizations between Zoho and Entity API database
+   * addressing deleted and unwanted (defined by search criteria) organizations. We separate to
+   * update from to delete types
+   * 
+   * @param orgList The list of retrieved Zoho objects
+   * @param toUpdateList The list of Zoho organizations to update
+   * @param toDeleteList The list of Zoho IDs to remove
+   */
+  private void fillDeletionAndUpdateLists(final List<ZohoOrganization> orgList,
+      List<ZohoOrganization> toUpdateList, List<String> toDeleteList) {
+
+    Organization dbOrganization;
+
+    for (ZohoOrganization org : orgList) {
+      /* validate Zoho organization */
+      boolean zohoOrganizationValidation = hasRequiredRole(org);
+      /* retrieve related organization from database */
+      dbOrganization = entityService
+          .getOrganizationById(ZohoAccessService.URL_ORGANIZATION_PREFFIX + org.getZohoId());
+      /* extract roles from organization objects and compare them */
+      if (dbOrganization != null && dbOrganization.getEdmEuropeanaRole() != null
+          && org.getRole() != null) {
+        if (zohoOrganizationValidation && !dbOrganization.getEdmEuropeanaRole()
+            .get(Locale.ENGLISH.getLanguage()).equals(org.getRole())) {
+          /*
+           * if roles in Zoho match to the filter criteria add organization to the update list if
+           * they are different
+           */
+          toUpdateList.add(org);
+        }
+        if (!zohoOrganizationValidation) {
+          /*
+           * if roles in Zoho do not match to the filter criteria add organization to the delete
+           * list
+           */
+          toDeleteList.add(ZohoAccessService.URL_ORGANIZATION_PREFFIX + org.getZohoId());
+        }
+      }
+    }
+  }
+
+  /**
+   * Retrieve deleted in Zoho organizations and removed from the Enrichment database
+   * 
+   * @return the number of deleted from Enrichment database organizations
+   * @throws ZohoAccessException
+   */
+  public int synchronizeDeletedZohoOrganizations() throws ZohoAccessException {
+
+    List<String> deletedZohoOrganizationList;
+    int MAX_ITEMS_PER_PAGE = 200;
+    int startPage = 1;
+    boolean hasNext = true;
+    int numberOfDeletedDbOrganizations = 0;
+    while (hasNext) {
+      deletedZohoOrganizationList = zohoAccessService.getDeletedOrganizations(startPage);
+      numberOfDeletedDbOrganizations += deleteOrganizations(deletedZohoOrganizationList);
+      startPage += 1;
+      // if no more organizations exist in Zoho
+      if (deletedZohoOrganizationList.size() < MAX_ITEMS_PER_PAGE)
+        hasNext = false;
+    }
+
+    return numberOfDeletedDbOrganizations;
   }
 
   private Map<String, String> buildSearchCriteria() {
@@ -147,41 +235,44 @@ public class OrganizationImporter {
   }
 
   /**
-   * This method validates that organization roles match to
-   * the filter criteria.
+   * This method validates that organization roles match to the filter criteria.
+   * 
    * @param orgList
    * @return filtered and validated orgnization list
    */
-  public boolean validateOrganizationsByFilterCriteria(
-      ZohoOrganization organization) {
-    
+  public boolean hasRequiredRole(ZohoOrganization organization) {
+
     boolean res = false;
-    
+
     if (searchCriteria == null || searchCriteria.isEmpty())
       return true;
-    
-    //need to fix Zoho Bugg in API
-    if(!searchCriteria.containsKey(ZohoApiFields.ORGANIZATION_ROLE))
+
+    // need to fix Zoho Bugg in API
+    if (!searchCriteria.containsKey(ZohoApiFields.ORGANIZATION_ROLE))
       return true;
-        
-    String[] organizationRoles = organization.getRole().split(ZohoApiFields.SEMICOLON);
-    for (String organizationRole : organizationRoles) {
-      if (allowedRoles.contains(organizationRole))
-        res = true;
-      else
-        LOGGER.warn("{}", "Ignoring organization " + organization.getZohoId()
-          + "as the role doesn't match the role criteria: " + organization.getRole());
+
+    if (organization.getRole() != null) {
+      String[] organizationRoles = organization.getRole().split(ZohoApiFields.SEMICOLON);
+      for (String organizationRole : organizationRoles) {
+        if (allowedRoles.contains(organizationRole)) {
+          res = true;
+          break;
+        } else {
+          LOGGER.warn("{}", "Ignoring organization " + organization.getZohoId()
+              + "as the role doesn't match the role criteria: " + organization.getRole());
+        }
+      }
     }
-    
+
     return res;
   }
-      
+
   private int storeOrganizations(List<ZohoOrganization> orgList) {
     int count = 0;
     Organization edmOrg;
     for (ZohoOrganization org : orgList) {
       try {
-        if (validateOrganizationsByFilterCriteria(org)) {
+        if (hasRequiredRole(org)) {
           edmOrg = zohoAccessService.toEdmOrganization(org);
           entityService.storeOrganization(edmOrg, org.getCreated(), org.getModified());
           count++;
@@ -194,18 +285,46 @@ public class OrganizationImporter {
     return count;
   }
 
+  /**
+   * This method removes organizations from database
+   * using IDs from given list that exist in database
+   * 
+   * @param orgList The list of organizations to remove
+   * @return The number of removed organizations
+   */
+  private int deleteOrganizations(List<String> orgList) {
+    int count = 0;
+
+    if (!orgList.isEmpty()) {
+      try {
+        // get organizations existing in database
+        List<String> dbOrgList = entityService.findExistingOrganizations(orgList);
+        if(!dbOrgList.isEmpty()) {
+          entityService.deleteOrganizations(dbOrgList);
+          count = dbOrgList.size(); 
+        }
+      } catch (Exception e) {
+        LOGGER.warn("Cannot delete organizations. ", e);
+      }
+    }
+    return count;
+  }
+
   public void init() throws Exception {
     Properties appProps = loadProperties(PROPERTIES_FILE);
     String zohoBaseUrl = appProps.getProperty("zoho.base.url");
+    String zohoBaseUrlV2 = appProps.getProperty("zoho.base.url.v2");
     LOGGER.info("using zoho base URL: " + zohoBaseUrl);
+    LOGGER.info("using zoho base URL V2: " + zohoBaseUrlV2);
     String token = appProps.getProperty("zoho.authentication.token");
     if (token == null || token.length() < 6)
       throw new IllegalArgumentException("zoho.authentication.token is invalid: " + token);
     LOGGER.info("using zoho zoho authentication token: " + token.substring(0, 3) + "...");
 
     ZohoAccessClientDao zohoAccessClientDao = new ZohoAccessClientDao(zohoBaseUrl, token);
+    ZohoV2AccessDao zohoV2AccessDao = new ZohoV2AccessDao(zohoBaseUrlV2, token);
 
-    zohoAccessService = new ZohoAccessService(zohoAccessClientDao);
+    zohoAccessService = new ZohoAccessService(zohoAccessClientDao, zohoV2AccessDao);
 
     String mongoHost = appProps.getProperty("mongo.hosts");
     int mongoPort = Integer.valueOf(appProps.getProperty("mongo.port"));
@@ -216,9 +335,10 @@ public class OrganizationImporter {
   }
 
   private void initSearchCriteria() {
-    //build allowed roles
+    // build allowed roles
     searchCriteria = buildSearchCriteria();
-    String[] roles = searchCriteria.get(ZohoApiFields.ORGANIZATION_ROLE).split(ZohoApiFields.DELIMITER_COMMA);
+    String[] roles =
+        searchCriteria.get(ZohoApiFields.ORGANIZATION_ROLE).split(ZohoApiFields.DELIMITER_COMMA);
     allowedRoles = new HashSet<String>();
     for (int i = 0; i < roles.length; i++) {
       allowedRoles.add(roles[i].trim());
