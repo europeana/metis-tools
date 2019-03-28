bd81cfb1d82fc81bda4f61f95498aec64c0864ef
gsergiu <sergiu.gordea@ait.ac.at>
2019-03-28 11:08:16 +0100

    #EA-1513 quickfix for organization and address storage and retrieval

---
+++ zoho-import/src/main/java/eu/europeana/corelib/solr/entity/AddressImpl.java
@@ -0,0 +1,82 @@
+package eu.europeana.corelib.solr.entity;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+import org.mongodb.morphia.annotations.Entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import eu.europeana.corelib.definitions.edm.entity.Address;
+
+@JsonSerialize(include = Inclusion.NON_EMPTY)
+@JsonInclude(NON_EMPTY)
+@Entity ("Address")
+public class AddressImpl implements Address {
+
+	private String about;
+	private String vcardStreetAddress;
+	private String vcardLocality;
+	private String vcardPostalCode;
+	private String vcardCountryName;
+	private String vcardPostOfficeBox;
+	private String vcardHasGeo;
+	
+	@Override
+	public String getAbout() {
+		return about;
+	}
+	@Override
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	@Override
+	public String getVcardStreetAddress() {
+		return vcardStreetAddress;
+	}
+	@Override
+	public void setVcardStreetAddress(String vcardStreetAddress) {
+		this.vcardStreetAddress = vcardStreetAddress;
+	}
+	@Override
+	public String getVcardLocality() {
+		return vcardLocality;
+	}
+	@Override
+	public void setVcardLocality(String vcardLocality) {
+		this.vcardLocality = vcardLocality;
+	}
+	@Override
+	public String getVcardPostalCode() {
+		return vcardPostalCode;
+	}
+	@Override
+	public void setVcardPostalCode(String vcardPostalCode) {
+		this.vcardPostalCode = vcardPostalCode;
+	}
+	@Override
+	public String getVcardCountryName() {
+		return vcardCountryName;
+	}
+	@Override
+	public void setVcardCountryName(String vcardCountryName) {
+		this.vcardCountryName = vcardCountryName;
+	}
+	@Override
+	public String getVcardPostOfficeBox() {
+		return vcardPostOfficeBox;
+	}
+	@Override
+	public void setVcardPostOfficeBox(String vcardPostOfficeBox) {
+		this.vcardPostOfficeBox = vcardPostOfficeBox;
+	}
+	@Override
+	public String getVcardHasGeo() {
+		return vcardHasGeo;
+	}
+	@Override
+	public void setVcardHasGeo(String vcardHasGeo) {
+		this.vcardHasGeo = vcardHasGeo;
+	}
+}
---
+++ zoho-import/src/main/java/eu/europeana/corelib/solr/entity/ContextualClassImpl.java
@@ -0,0 +1,76 @@
+package eu.europeana.corelib.solr.entity;
+
+import java.util.List;
+import java.util.Map;
+
+import eu.europeana.corelib.definitions.edm.entity.ContextualClass;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+
+public class ContextualClassImpl extends AbstractEdmEntityImpl implements ContextualClass {
+	
+	private Map<String,List<String>> prefLabel;
+	private Map<String,List<String>> altLabel;
+	private Map<String,List<String>> hiddenLabel;
+	private Map<String,List<String>> note;
+	private String foafDepiction;
+	
+
+	@Override
+	public Map<String, List<String>> getPrefLabel() {
+		return this.prefLabel;
+	}
+
+	@Override
+	public Map<String, List<String>> getHiddenLabel() {
+		return this.hiddenLabel;
+	}
+
+	@Override
+	public Map<String, List<String>> getAltLabel() {
+		return this.altLabel;
+	}
+
+	@Override
+	public Map<String,List<String>> getNote() {
+		return this.note;
+	}
+
+	@Override
+	public void setAltLabel(Map<String, List<String>> altLabel) {
+		this.altLabel = altLabel;
+	}
+
+	@Override
+	public void setPrefLabel(Map<String, List<String>> prefLabel) {
+		this.prefLabel = prefLabel;
+	}
+
+	@Override
+	public void setHiddenLabel(Map<String, List<String>> hiddenLabel) {
+		this.hiddenLabel = hiddenLabel;
+	}
+
+	@Override
+	public void setNote(Map<String,List<String>>note) {
+		this.note = note;
+	}
+
+	@Override
+	public String getFoafDepiction() {
+		return foafDepiction;
+	}
+
+	@Override
+	public void setFoafDepiction(String foafDepiction) {
+		this.foafDepiction = foafDepiction;
+	}
+
+	@JsonIgnore
+	@com.fasterxml.jackson.annotation.JsonIgnore
+	@Override
+	public String getEntityIdentifier() {
+		String[] splitArray = this.getAbout().split("/");
+		return splitArray[splitArray.length-1];
+	}
+}
