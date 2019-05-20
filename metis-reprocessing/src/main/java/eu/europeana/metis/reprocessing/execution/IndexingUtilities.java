196623dbda2ab8cc19c11663358597808a2af47f
stzanakis <simon.tzanakis@europeana.eu>
2019-05-20 10:50:47 +0200

    MET-1887 Retry on zookeeper session expired

--- metis-reprocessing/src/main/java/eu/europeana/metis/reprocessing/execution/IndexingUtilities.java
+++ metis-reprocessing/src/main/java/eu/europeana/metis/reprocessing/execution/IndexingUtilities.java
@@ -5,12 +5,19 @@ import eu.europeana.indexing.Indexer;
 import eu.europeana.indexing.exception.IndexingException;
 import eu.europeana.indexing.exception.RecordRelatedIndexingException;
 import eu.europeana.metis.reprocessing.model.BasicConfiguration;
+import eu.europeana.metis.utils.ExternalRequestUtil;
+import java.util.Collections;
+import java.util.Map;
+import org.apache.zookeeper.KeeperException.SessionExpiredException;
 
 /**
  * @author Simon Tzanakis (Simon.Tzanakis@europeana.eu)
  * @since 2019-05-17
  */
 public class IndexingUtilities {
+
+  private static final Map<Class<?>, String> exceptionMap = Collections
+      .singletonMap(SessionExpiredException.class, "Session expired");
 
   private IndexingUtilities() {
   }
@@ -19,8 +26,11 @@ public class IndexingUtilities {
       BasicConfiguration basicConfiguration) throws IndexingException {
     try {
       final Indexer indexer = basicConfiguration.getIndexer();
-      indexer.indexRdf(rdf, preserveTimestamps);
-    } catch (RuntimeException e) {
+      ExternalRequestUtil.retryableExternalRequest(() -> {
+        indexer.indexRdf(rdf, preserveTimestamps);
+        return null;
+      }, exceptionMap);
+    } catch (Exception e) {
       throw new RecordRelatedIndexingException("A Runtime Exception occurred", e);
     }
   }
