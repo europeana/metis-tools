197ce36189da3714ccce7adfa2c272603547f1e5
jbrek <jbrek@man.poznan.pl>
2021-02-08 13:39:40 +0100

    review fix + move forward if couldnt resolve host

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-# Check how many OAI repositories used in Metis support Deleted tag
-set -e
+# Check how many OAI repositories used in Metis support deleted/persistent/transient tag
 PATH_TO_FILE_WITH_URLS=$1
-xargs -r < ${PATH_TO_FILE_WITH_URLS} curl | grep -o "<deletedRecord>no</deletedRecord>" | wc -l > supported-repositories-no.txt
+
+NO_PATTERN="<deletedRecord>no</deletedRecord>"
+PERSISTENT_PATTERN="<deletedRecord>persistent</deletedRecord>"
+TRANSIENT_PATTERN="<deletedRecord>transient</deletedRecord>"
+
+RESPONSE=$(xargs -r < ${PATH_TO_FILE_WITH_URLS} curl)
+
+REPOSITORIES_NO_NO=$(echo ${RESPONSE} | grep -o ${NO_PATTERN} | wc -l)
+REPOSITORIES_PERSISTENT_NO=$(echo ${RESPONSE} | grep -o ${PERSISTENT_PATTERN} | wc -l)
+REPOSITORIES_TRANSIENT_NO=$(echo ${RESPONSE} | grep -o ${TRANSIENT_PATTERN} | wc -l)
+
+echo ${NO_PATTERN} ${REPOSITORIES_NO_NO}
+echo ${PERSISTENT_PATTERN} ${REPOSITORIES_PERSISTENT_NO}
+echo ${TRANSIENT_PATTERN} ${REPOSITORIES_TRANSIENT_NO}
