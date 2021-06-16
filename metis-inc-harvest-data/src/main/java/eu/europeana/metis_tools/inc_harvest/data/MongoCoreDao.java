801892f4e1f0509871ba22e039d751a2f591bed5
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2021-06-16 19:04:24 +0200

    MET-3656: Process review comments.

--- metis-inc-harvest-data/src/main/java/eu/europeana/metis_tools/inc_harvest/data/MongoCoreDao.java
+++ metis-inc-harvest-data/src/main/java/eu/europeana/metis_tools/inc_harvest/data/MongoCoreDao.java
@@ -3,10 +3,7 @@ package eu.europeana.metis_tools.inc_harvest.data;
 import static eu.europeana.metis.core.common.DaoFieldNames.DATASET_ID;
 import static eu.europeana.metis.core.common.DaoFieldNames.ID;
 
-import dev.morphia.aggregation.experimental.Aggregation;
-import dev.morphia.aggregation.experimental.expressions.Expressions;
-import dev.morphia.aggregation.experimental.stages.Projection;
-import dev.morphia.annotations.Entity;
+import dev.morphia.query.FindOptions;
 import eu.europeana.metis.core.dataset.Dataset;
 import eu.europeana.metis.core.mongo.MorphiaDatastoreProvider;
 import eu.europeana.metis.core.mongo.MorphiaDatastoreProviderImpl;
@@ -28,35 +25,14 @@ public class MongoCoreDao {
   }
 
   public Stream<String> getAllDatasetIds() {
-
     return ExternalRequestUtil.retryableExternalRequestForNetworkExceptions(() -> {
-
-      // Create aggregation pipeline finding all datasets.
-      final Aggregation<Dataset> pipeline = datastoreProvider
-              .getDatastore().aggregate(eu.europeana.metis.core.dataset.Dataset.class);
-
-      // The field name should be the field name in DatasetIdWrapper.
-      final String datasetIdField = "datasetId";
-
-      // Project the dataset ID to the right field name.
-      pipeline.project(Projection.project().exclude(ID.getFieldName())
-              .include(datasetIdField, Expressions.field(DATASET_ID.getFieldName())));
-
-      // Perform the aggregation and add the IDs in the result set.
-      final Iterator<DatasetIdWrapper> resultIterator = pipeline.execute(DatasetIdWrapper.class);
+      final FindOptions findOptions = new FindOptions()
+              .projection().exclude(ID.getFieldName())
+              .projection().include(DATASET_ID.getFieldName());
+      final Iterator<Dataset> resultIterator = datastoreProvider.getDatastore().find(Dataset.class)
+              .iterator(findOptions);
       return StreamSupport.stream(Spliterators.spliteratorUnknownSize(resultIterator, 0), false)
-              .map(DatasetIdWrapper::getDatasetId);
+              .map(Dataset::getDatasetId);
     });
-  }
-
-  @Entity
-  private static class DatasetIdWrapper {
-
-    // Name depends on the mongo aggregations in which it is used.
-    private String datasetId;
-
-    String getDatasetId() {
-      return datasetId;
-    }
   }
 }
--- metis-inc-harvest-data/src/main/java/eu/europeana/metis_tools/inc_harvest/data/MongoRecordDao.java
+++ metis-inc-harvest-data/src/main/java/eu/europeana/metis_tools/inc_harvest/data/MongoRecordDao.java
@@ -2,10 +2,7 @@ package eu.europeana.metis_tools.inc_harvest.data;
 
 import static eu.europeana.metis.core.common.DaoFieldNames.ID;
 
-import dev.morphia.aggregation.experimental.Aggregation;
-import dev.morphia.aggregation.experimental.expressions.Expressions;
-import dev.morphia.aggregation.experimental.stages.Projection;
-import dev.morphia.annotations.Entity;
+import dev.morphia.query.FindOptions;
 import dev.morphia.query.experimental.filters.Filters;
 import eu.europeana.corelib.solr.bean.impl.FullBeanImpl;
 import eu.europeana.metis.mongo.connection.MongoClientProvider;
@@ -28,39 +25,18 @@ public class MongoRecordDao {
     recordDao = new RecordDao(mongoClientProvider.createMongoClient(), mongoCoreDb);
   }
 
-  public Stream<String> getAllRecordIds(String datasetId){
+  public Stream<String> getAllRecordIds(String datasetId) {
     final Pattern pattern = Pattern.compile("^/" + Pattern.quote(datasetId) + "/");
     return ExternalRequestUtil.retryableExternalRequestForNetworkExceptions(() -> {
-
-      // Create aggregation pipeline finding all datasets.
-      final Aggregation<FullBeanImpl> pipeline = recordDao.getDatastore()
-              .aggregate(FullBeanImpl.class);
-
-      // Filter on dataset ID
-      pipeline.match(Filters.regex(ABOUT_FIELD).pattern(pattern));
-
-      // The field name should be the field name in RecordIdWrapper.
-      final String recordIdField = "recordId";
-
-      // Project the dataset ID to the right field name.
-      pipeline.project(Projection.project().exclude(ID.getFieldName())
-              .include(recordIdField, Expressions.field(ABOUT_FIELD)));
-
-      // Perform the aggregation and add the IDs in the result set.
-      final Iterator<RecordIdWrapper> resultIterator = pipeline.execute(RecordIdWrapper.class);
+      final FindOptions findOptions = new FindOptions()
+              .projection().exclude(ID.getFieldName())
+              .projection().include(ABOUT_FIELD);
+      final Iterator<FullBeanImpl> resultIterator = recordDao.getDatastore()
+              .find(FullBeanImpl.class)
+              .filter(Filters.regex(ABOUT_FIELD).pattern(pattern))
+              .iterator(findOptions);
       return StreamSupport.stream(Spliterators.spliteratorUnknownSize(resultIterator, 0), false)
-              .map(RecordIdWrapper::getRecordId);
+              .map(FullBeanImpl::getAbout);
     });
-  }
-
-  @Entity
-  private static class RecordIdWrapper {
-
-    // Name depends on the mongo aggregations in which it is used.
-    private String recordId;
-
-    public String getRecordId() {
-      return recordId;
-    }
   }
 }
