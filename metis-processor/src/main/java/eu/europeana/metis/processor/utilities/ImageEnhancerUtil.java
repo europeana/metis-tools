6921b99a2e8bf4a758d540f2526d6cd3d71ef071
Jorge Ortiz <jorge.ortiz@europeana.eu>
2023-08-04 09:20:45 +0200

    Feat/mets processor poc image enhancer media processing update (#112)
    * Added media processing part
    
    * remove file

--- metis-processor/src/main/java/eu/europeana/metis/processor/utilities/ImageEnhancerUtil.java
+++ metis-processor/src/main/java/eu/europeana/metis/processor/utilities/ImageEnhancerUtil.java
@@ -2,15 +2,27 @@ package eu.europeana.metis.processor.utilities;
 
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import eu.europeana.metis.image.enhancement.client.ImageEnhancerClient;
+import eu.europeana.metis.image.enhancement.config.ImageEnhancerClientConfig;
 import eu.europeana.metis.mediaprocessing.exception.MediaExtractionException;
+import eu.europeana.metis.mediaprocessing.exception.MediaProcessorException;
+import eu.europeana.metis.mediaprocessing.extraction.CommandExecutor;
+import eu.europeana.metis.mediaprocessing.extraction.ImageMetadata;
+import eu.europeana.metis.mediaprocessing.extraction.ThumbnailGenerator;
+import eu.europeana.metis.mediaprocessing.model.Thumbnail;
 import eu.europeana.metis.mediaprocessing.model.ThumbnailKind;
 import eu.europeana.metis.schema.jibx.RDF;
 import eu.europeana.metis.schema.jibx.WebResourceType;
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -40,17 +52,16 @@ public class ImageEnhancerUtil {
      */
     public void processRecord(RDF recordToProcess) {
         final long startTime = System.nanoTime();
-        extractThumbnailEnhanceAndReuploadIt(recordToProcess);
+        extractThumbnailEnhanceAndReUploadIt(recordToProcess);
         final long elapsedTimeInNanoSec = System.nanoTime() - startTime;
         LOGGER.info("elapsed time for whole processing {}ns", elapsedTimeInNanoSec);
     }
 
-    private void extractThumbnailEnhanceAndReuploadIt(RDF recordToProcess) {
+    private void extractThumbnailEnhanceAndReUploadIt(RDF recordToProcess) {
 
         List<WebResourceType> thumbnailResourceList = recordToProcess.getWebResourceList()
                 .stream()
-                .filter(thumbnailResource -> thumbnailResource.getHasMimeType().getHasMimeType().equals("image/jpeg")
-                        || thumbnailResource.getHasMimeType().getHasMimeType().equals("image/png"))
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
 
         thumbnailResourceList.forEach(thumbnailResource -> {
@@ -59,27 +70,70 @@ public class ImageEnhancerUtil {
                 final String largeThumbnailObjectName = ThumbnailUtil.getThumbnailName(thumbnailResource.getAbout(), ThumbnailKind.LARGE);
                 if (hasThumbnailResolutionLowerThan400(thumbnailResource)) {
                     LOGGER.info("{}\t=>\t{}", thumbnailResource.getAbout(), largeThumbnailObjectName);
-                    byte[] largeThumbnail = s3Client.getObject(largeThumbnailObjectName);
-                    byte[] enhancedLargeThumbnailData = this.imageEnhancerClient.enhance(largeThumbnail);
-                    // do media processing here
-//                    s3Client.putObject(largeThumbnailObjectName, new ByteArrayInputStream(enhancedLargeThumbnailData), prepareObjectMetadata(thumbnailResource));
+                    byte[] enhancedLargeThumbnailData = enhanceThumbnail(thumbnailResource, largeThumbnailObjectName);
+                    // enhancedLargeThumbnail comes with media processed info
+                    s3Client.putObject(largeThumbnailObjectName, new ByteArrayInputStream(enhancedLargeThumbnailData), prepareObjectMetadata(thumbnailResource));
                     if (hasThumbnailResolutionLowerThan200(thumbnailResource)) {
                         LOGGER.info("{}\t=>\t{}", thumbnailResource.getAbout(), smallThumbnailObjectName);
-                        // do media processing here
-                        //s3Client.putObject(smallThumbnailObjectName, new ByteArrayInputStream(enhancedSmallThumbnailData), prepareObjectMetadata(thumbnailResource));
+                        byte[] enhancedSmallThumbnailData = enhanceThumbnail(thumbnailResource, smallThumbnailObjectName);
+                        // enhancedSmallThumbnail comes with media processed info
+                        s3Client.putObject(smallThumbnailObjectName, new ByteArrayInputStream(enhancedSmallThumbnailData), prepareObjectMetadata(thumbnailResource));
                     }
                 }
-            } catch (MediaExtractionException e) {
+            } catch (MediaExtractionException | IOException e) {
                 LOGGER.error("enhancing thumbnail image {} {}", thumbnailResource.getAbout(), e);
             }
         });
     }
+    private byte[] enhanceThumbnail(WebResourceType resource, String thumbnailName) throws IOException {
+        byte[] result = this.s3Client.getObject(thumbnailName);
+        String fileExtension = "";
+        if ((resource.getHasMimeType() != null && resource.getHasMimeType().getHasMimeType().equals("image/jpeg"))
+                || resource.getAbout().substring(resource.getAbout().lastIndexOf(".") + 1).equals("jpg")
+                || resource.getAbout().substring(resource.getAbout().lastIndexOf(".") + 1).equals("jpeg")
+        ) {
+            fileExtension = ".jpeg";
+
+        }
+        if ((resource.getHasMimeType() != null && resource.getHasMimeType().getHasMimeType().equals("image/png"))
+                || resource.getAbout().substring(resource.getAbout().lastIndexOf(".") + 1).equals("png")) {
+            fileExtension = ".png";
+        }
+        final String fileName = "/tmp/" + thumbnailName + fileExtension;
+        try (FileOutputStream fos = new FileOutputStream("/tmp/" + thumbnailName + fileExtension)) {
+            fos.write(result);
+        } catch (IOException e) {
+            LOGGER.error("writing temp file");
+        }
+        byte[] enhancedImage = this.imageEnhancerClient.enhance(result);
+        try {
+            ThumbnailGenerator thumbnailGenerator = new ThumbnailGenerator(new CommandExecutor(300));
+            Pair<ImageMetadata, List<Thumbnail>> output = thumbnailGenerator.generateThumbnails(thumbnailName, resource.getHasMimeType().getHasMimeType(), new File(fileName), true);
+            List<Thumbnail> resizedThumbnails = output.getRight();
+            for (Thumbnail thumbnail : resizedThumbnails) {
+                // final size
+                enhancedImage = thumbnail.getContentStream().readAllBytes();
+                try (FileOutputStream fos = new FileOutputStream("/tmp/" + thumbnail.getTargetName() + fileExtension)) {
+                    fos.write(enhancedImage);
+                } catch (IOException e) {
+                    LOGGER.error("writing temp file");
+                }
+            }
+        } catch (MediaProcessorException | MediaExtractionException e) {
+            LOGGER.error("running extracting media", e);
+        }
+
+        return enhancedImage;
+    }
+
     private boolean hasThumbnailResolutionLowerThan400(WebResourceType thumbnailResource) {
-        return Math.min(thumbnailResource.getHeight().getLong(), thumbnailResource.getWidth().getLong()) < 400;
+        return thumbnailResource.getHeight() != null && thumbnailResource.getWidth() != null &&
+                Math.min(thumbnailResource.getHeight().getLong(), thumbnailResource.getWidth().getLong()) < 400;
     }
 
     private boolean hasThumbnailResolutionLowerThan200(WebResourceType thumbnailResource) {
-        return Math.min(thumbnailResource.getHeight().getLong(), thumbnailResource.getWidth().getLong()) < 200;
+        return thumbnailResource.getHeight() != null && thumbnailResource.getWidth() != null &&
+                Math.min(thumbnailResource.getHeight().getLong(), thumbnailResource.getWidth().getLong()) < 200;
     }
 
     private ObjectMetadata prepareObjectMetadata(WebResourceType thumbnail) {
