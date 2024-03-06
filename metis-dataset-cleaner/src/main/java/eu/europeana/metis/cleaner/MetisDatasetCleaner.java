861b4322afa51f84b177310866c3e4b757968743
Jorge Ortiz <jorge.ortiz@europeana.eu>
2024-03-06 11:19:31 +0100

    MET-5837 add option of remove by dataset or record id

--- metis-dataset-cleaner/src/main/java/eu/europeana/metis/cleaner/MetisDatasetCleaner.java
+++ metis-dataset-cleaner/src/main/java/eu/europeana/metis/cleaner/MetisDatasetCleaner.java
@@ -1,7 +1,9 @@
 package eu.europeana.metis.cleaner;
 
 import eu.europeana.metis.cleaner.common.TargetIndexingDatabase;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
@@ -22,28 +24,38 @@ public class MetisDatasetCleaner implements ApplicationRunner {
   public void run(ApplicationArguments args) throws Exception {
     LOGGER.info("Starting cleaning database script");
     ApplicationInitializer applicationInitializer = new ApplicationInitializer();
-    // Help
-    LOGGER.info("Usage: where # is record id number.");
-    LOGGER.info("mvn spring-boot:run -Dspring-boot.run.arguments=\"--record.id=#\"");
-    LOGGER.info("java -jar metis-dataset-cleaner-1.0-SNAPSHOT.jar --record.id=#");
-    // Reading Command-Line arguments
+    // Usage help
+    LOGGER.info("Usage: where # is record or dataset id number.");
+    LOGGER.info("mvn spring-boot:run -Dspring-boot.run.arguments=\"--record.id=#\" or \"--dataset.id=#\"");
+    LOGGER.info("java -jar metis-dataset-cleaner-1.0-SNAPSHOT.jar --record.id=# or --dataset.id=#");
+
+    // Reading command-Line arguments
     LOGGER.info("Application started with command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
     LOGGER.info("NonOptionArgs: {}", args.getNonOptionArgs());
     LOGGER.info("OptionNames: {}", args.getOptionNames());
 
     for (String name : args.getOptionNames()) {
-      LOGGER.info("arg-" + name + "=" + args.getOptionValues(name));
+      LOGGER.info("arg-{}={}",name, args.getOptionValues(name));
     }
 
-    boolean containsOption = args.containsOption("record.id");
-    LOGGER.info("Contains record.id: " + containsOption);
-
-    if (containsOption) {
+    final boolean containsRecordId = args.containsOption("record.id");
+    if (containsRecordId) {
+      LOGGER.info("::Contains record.id::");
       final String recordId = args.getOptionValues("record.id").getFirst();
       LOGGER.info("cleaning preview record.id: {}", recordId);
       applicationInitializer.getIndexWrapper().getIndexer(TargetIndexingDatabase.PREVIEW).remove(recordId);
       LOGGER.info("cleaning publish record.id: {}", recordId);
       applicationInitializer.getIndexWrapper().getIndexer(TargetIndexingDatabase.PUBLISH).remove(recordId);
+    }
+
+    final boolean containsDatasetId = args.containsOption("dataset.id");
+    if (containsDatasetId) {
+      LOGGER.info("::Contains dataset.id::");
+      final String datasetId = args.getOptionValues("dataset.id").getFirst();
+      LOGGER.info("cleaning preview dataset.id: {}", datasetId);
+      applicationInitializer.getIndexWrapper().getIndexer(TargetIndexingDatabase.PREVIEW).removeAll(datasetId, Date.from(Instant.now()));
+      LOGGER.info("cleaning publish dataset.id: {}", datasetId);
+      applicationInitializer.getIndexWrapper().getIndexer(TargetIndexingDatabase.PUBLISH).removeAll(datasetId, Date.from(Instant.now()));
     }
     LOGGER.info("Finished cleaning database script");
     System.exit(0);
