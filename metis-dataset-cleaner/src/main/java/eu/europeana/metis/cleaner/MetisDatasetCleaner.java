cc886ef0f81f7e66931aa4c6bc5ba9a0db928910
Jorge Ortiz <jorge.ortiz@europeana.eu>
2024-03-12 16:20:21 +0100

    MET-5837 add option of indexing a record via file

--- metis-dataset-cleaner/src/main/java/eu/europeana/metis/cleaner/MetisDatasetCleaner.java
+++ metis-dataset-cleaner/src/main/java/eu/europeana/metis/cleaner/MetisDatasetCleaner.java
@@ -1,9 +1,13 @@
 package eu.europeana.metis.cleaner;
 
+import eu.europeana.indexing.IndexingProperties;
 import eu.europeana.metis.cleaner.common.TargetIndexingDatabase;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Date;
+import jnr.ffi.annotations.In;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
@@ -26,8 +30,10 @@ public class MetisDatasetCleaner implements ApplicationRunner {
     ApplicationInitializer applicationInitializer = new ApplicationInitializer();
     // Usage help
     LOGGER.info("Usage: where # is record or dataset id number.");
-    LOGGER.info("mvn spring-boot:run -Dspring-boot.run.arguments=\"--record.id=#\" or \"--dataset.id=#\"");
+    LOGGER.info("       where X is path to a rdf xml record ready for preview and publish.");
+    LOGGER.info("mvn spring-boot:run -Dspring-boot.run.arguments=\"--record.id=#\" or \"--dataset.id=#\" or --index.file=\"X\"");
     LOGGER.info("java -jar metis-dataset-cleaner-1.0-SNAPSHOT.jar --record.id=# or --dataset.id=#");
+    LOGGER.info("java -jar metis-dataset-cleaner-1.0-SNAPSHOT.jar --index.file=\"X\"");
 
     // Reading command-Line arguments
     LOGGER.info("Application started with command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
@@ -57,6 +63,22 @@ public class MetisDatasetCleaner implements ApplicationRunner {
       LOGGER.info("cleaning publish dataset.id: {}", datasetId);
       applicationInitializer.getIndexWrapper().getIndexer(TargetIndexingDatabase.PUBLISH).removeAll(datasetId, Date.from(Instant.now()));
     }
+
+    final boolean containsRecordFile = args.containsOption("index.file");
+    if (containsRecordFile) {
+      LOGGER.info("::Contains index.file::");
+      final String fileName = args.getOptionValues("index.file").getFirst();
+      try(FileInputStream fileInputStream = new FileInputStream(fileName)) {
+        IndexingProperties indexingProperties = new IndexingProperties(Date.from(Instant.now()), true, null, true, true);
+        LOGGER.info("indexing preview file: {}", fileName);
+        applicationInitializer.getIndexWrapper().getIndexer(TargetIndexingDatabase.PREVIEW)
+                              .index(fileInputStream, indexingProperties);
+        LOGGER.info("indexing publish file: {}", fileName);
+        applicationInitializer.getIndexWrapper().getIndexer(TargetIndexingDatabase.PUBLISH)
+                              .index(fileInputStream, indexingProperties);
+      }
+    }
+
     LOGGER.info("Finished cleaning database script");
     System.exit(0);
   }
