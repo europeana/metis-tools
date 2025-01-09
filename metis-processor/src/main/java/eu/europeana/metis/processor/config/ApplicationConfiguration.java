547264848401c6f6b86bb9e65a96ace75b7ec322
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-01-09 16:11:19 +0100

    MET-6262 set version of morphia and mongodb driver to run correctly the processor

--- metis-processor/src/main/java/eu/europeana/metis/processor/config/ApplicationConfiguration.java
+++ metis-processor/src/main/java/eu/europeana/metis/processor/config/ApplicationConfiguration.java
@@ -24,7 +24,7 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 import org.apache.commons.lang3.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
