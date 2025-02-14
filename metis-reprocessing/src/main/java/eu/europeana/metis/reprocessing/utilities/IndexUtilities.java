7cf9c384a4a1de6f2813ef0b3ffbf9193144e15b
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-02-14 18:00:10 +0100

    MET-6260_MET-6362 Maintenance dataset 2048087

--- metis-reprocessing/src/main/java/eu/europeana/metis/reprocessing/utilities/IndexUtilities.java
+++ metis-reprocessing/src/main/java/eu/europeana/metis/reprocessing/utilities/IndexUtilities.java
@@ -1,19 +1,27 @@
 package eu.europeana.metis.reprocessing.utilities;
 
+import static java.util.function.Predicate.not;
 import static org.apache.commons.lang3.BooleanUtils.isFalse;
 
 import com.mongodb.MongoWriteException;
+import eu.europeana.corelib.solr.bean.impl.FullBeanImpl;
+import eu.europeana.corelib.solr.entity.AggregationImpl;
+import eu.europeana.corelib.solr.entity.OrganizationImpl;
+import eu.europeana.corelib.solr.entity.ProxyImpl;
 import eu.europeana.indexing.IndexerPool;
 import eu.europeana.indexing.IndexingProperties;
 import eu.europeana.indexing.exception.IndexingException;
 import eu.europeana.indexing.exception.RecordRelatedIndexingException;
+import eu.europeana.indexing.fullbean.RdfToFullBeanConverter;
 import eu.europeana.indexing.tiers.TierCalculationMode;
 import eu.europeana.indexing.tiers.model.MediaTier;
 import eu.europeana.indexing.utils.RdfTier;
 import eu.europeana.indexing.utils.RdfTierUtils;
+import eu.europeana.indexing.utils.RdfWrapper;
 import eu.europeana.metis.network.ExternalRequestUtil;
 import eu.europeana.metis.reprocessing.config.Configuration;
 import eu.europeana.metis.schema.convert.RdfConversionUtils;
+import eu.europeana.metis.schema.convert.SerializationException;
 import eu.europeana.metis.schema.jibx.Aggregation;
 import eu.europeana.metis.schema.jibx.EdmType;
 import eu.europeana.metis.schema.jibx.EuropeanaType;
@@ -24,10 +32,13 @@ import eu.europeana.metis.schema.jibx.RDF;
 import eu.europeana.metis.schema.jibx.ResourceOrLiteralType;
 import eu.europeana.metis.schema.jibx.Type2;
 import eu.europeana.metis.utils.DepublicationReason;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -36,7 +47,14 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Contains functionality for indexing.
@@ -49,6 +67,16 @@ import org.apache.commons.lang3.StringUtils;
 public class IndexUtilities {
 
   private static final Map<Class<?>, String> retryExceptions;
+  private static final Logger LOGGER = LoggerFactory.getLogger(IndexUtilities.class);
+  private static final List<String> DATA_PROVIDERS = List.of(
+      "Burns Scotland",
+      "Royal Albert Memorial Museum & Art Gallery",
+      "University College London",
+      "Egypt Centre",
+      "Wiltshire Treasures",
+      "Wakefield Council",
+      "Horniman Museum and Gardens",
+      "Battersea Arts Centre");
 
   static {
     retryExceptions = new HashMap<>(ExternalRequestUtil.UNMODIFIABLE_MAP_WITH_NETWORK_EXCEPTIONS);
@@ -97,6 +125,7 @@ public class IndexUtilities {
               && tierData.contains(RdfTier.CONTENT_TIER_1.getUri()))
               || (datasetId.equals("9200579") && hasDcCreator(rdf))
               || (datasetId.equals("2048128") && hasEdmType3D(rdf))
+              || (datasetId.equals("2048087") && hasDataProviders(rdf, DATA_PROVIDERS))
           ) {
             indexerPool.indexTombstone(stringRdf, DepublicationReason.GENERIC);
             indexerPool.remove(stringRdf);
@@ -106,6 +135,89 @@ public class IndexUtilities {
       }, retryExceptions);
     } catch (Exception e) {
       throw new RecordRelatedIndexingException("A Runtime Exception occurred while indexing record", e);
+    }
+  }
+
+  public static void main(String[] args) throws SerializationException {
+    final RdfConversionUtils conversionUtils = new RdfConversionUtils();
+    RDF inputRdf = conversionUtils.convertStringToRdf(
+        IndexingTestUtils.getResourceFileContent("test_record.xml"));
+    hasDataProviders(inputRdf, DATA_PROVIDERS);
+  }
+
+  private static Pair<String, String> findPrefLabelForOrganization(OrganizationImpl organization) {
+
+    // Try to find an English one first.
+    final List<Pair<String, String>> englishValues = new ArrayList<>(2);
+    Optional.ofNullable(organization.getPrefLabel()).map(labels -> labels.get("en")).stream()
+            .flatMap(List::stream).filter(Objects::nonNull).findFirst()
+            .ifPresent(value -> englishValues.add(new ImmutablePair<>("en", value)));
+    Optional.ofNullable(organization.getPrefLabel()).map(labels -> labels.get("eng")).stream()
+            .flatMap(List::stream).filter(Objects::nonNull).findFirst()
+            .ifPresent(value -> englishValues.add(new ImmutablePair<>("eng", value)));
+    if (!englishValues.isEmpty()) {
+      return englishValues.getFirst();
+    }
+
+    // Otherwise return any value (if available).
+    return Optional.ofNullable(organization.getPrefLabel()).map(Map::entrySet).stream()
+                   .flatMap(Collection::stream)
+                   .filter(Objects::nonNull).filter(entry -> entry.getValue() != null)
+                   .flatMap(entry -> entry.getValue().stream().filter(StringUtils::isNotBlank)
+                                          .map(value -> new ImmutablePair<>(entry.getKey(), value)))
+                   .findFirst().orElse(null);
+  }
+
+  private static List<AggregationImpl> getDataProviderAggregations(FullBeanImpl fullBean) {
+    List<String> proxyInResult = fullBean.getProxies().stream()
+                                         .filter(not(ProxyImpl::isEuropeanaProxy))
+                                         .filter(proxy -> ArrayUtils.isEmpty(proxy.getLineage())).map(ProxyImpl::getProxyIn)
+                                         .map(Arrays::asList).flatMap(List::stream).toList();
+
+    return fullBean.getAggregations().stream().filter(x -> proxyInResult.contains(x.getAbout())).toList();
+  }
+
+  private static Pair<Set<String>, Map<String, List<String>>> extractUrisAndLiterals(
+      final Map<String, List<String>> urisLiteralsMap, Map<String, Pair<String, String>> organizationPrefLabelMap) {
+    final Set<String> organizationUris = new HashSet<>();
+    final Map<String, List<String>> literalsMap = new HashMap<>();
+
+    if (MapUtils.isNotEmpty(urisLiteralsMap)) {
+      splitOrganizationUrisFromLiterals(urisLiteralsMap, organizationUris, literalsMap, organizationPrefLabelMap);
+
+      //Extend map with organization pref labels
+      if (CollectionUtils.isNotEmpty(organizationUris)) {
+        addOrganizationPrefLabelsToLiterals(organizationUris, literalsMap, organizationPrefLabelMap);
+      }
+    }
+    return new ImmutablePair<>(organizationUris, literalsMap);
+  }
+
+  private static void splitOrganizationUrisFromLiterals(Map<String, List<String>> urisLiteralsMap,
+      Set<String> organizationUris, Map<String, List<String>> literalsMap,
+      Map<String, Pair<String, String>> organizationPrefLabelMap) {
+    for (Map.Entry<String, List<String>> entry : urisLiteralsMap.entrySet()) {
+      final List<String> literals = new ArrayList<>();
+      for (String value : entry.getValue()) {
+        if (organizationPrefLabelMap.containsKey(value)) {
+          organizationUris.add(value);
+        } else {
+          literals.add(value);
+        }
+      }
+      if (!literals.isEmpty()) {
+        literalsMap.put(entry.getKey(), literals);
+      }
+    }
+  }
+
+  private static void addOrganizationPrefLabelsToLiterals(Set<String> organizationUris,
+      Map<String, List<String>> literalsMap, Map<String, Pair<String, String>> organizationPrefLabelMap) {
+    for (String organizationUri : organizationUris) {
+      final Pair<String, String> entry = organizationPrefLabelMap.get(organizationUri);
+      if (entry != null) {
+        literalsMap.computeIfAbsent(entry.getKey(), key -> new ArrayList<>()).add(entry.getValue());
+      }
     }
   }
 
@@ -138,8 +250,13 @@ public class IndexUtilities {
         Choice::ifCreator,
         Choice::getCreator,
         ResourceOrLiteralType::getString);
-
-    return creators.contains("Science Museum, London");
+    boolean result = creators.contains("Science Museum, London");
+    if (result) {
+      LOGGER.info("Has DC creator: {}", creators);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   static boolean isProviderProxy(ProxyType proxy) {
@@ -176,9 +293,48 @@ public class IndexUtilities {
                                     .collect(Collectors.toSet());
       if (types.size() == 1) {
         EdmType type = types.iterator().next();
-        return type.equals(EdmType._3_D);
+        boolean result = type.equals(EdmType._3_D);
+        if (result) {
+          LOGGER.info("Has EdmType3d: {}", type);
+          return true;
+        } else {
+          return false;
+        }
       }
     }
     return false;
   }
+
+  static boolean hasDataProviders(RDF rdf, List<String> providersList) {
+    RdfToFullBeanConverter rdfToFullBeanConverter = new RdfToFullBeanConverter();
+    FullBeanImpl fullBean = rdfToFullBeanConverter.convertRdfToFullBean(new RdfWrapper(rdf));
+
+    final Map<String, Pair<String, String>> organizationPrefLabelMap = fullBean.getOrganizations().stream()
+                                                                               .filter(
+                                                                                   org -> StringUtils.isNotBlank(org.getAbout()))
+                                                                               .collect(
+                                                                                   Collectors.toMap(OrganizationImpl::getAbout,
+                                                                                       IndexUtilities::findPrefLabelForOrganization,
+                                                                                       (o1, o2) -> o1));
+
+    AggregationImpl aggregation = getDataProviderAggregations(fullBean).getFirst();
+
+    final Pair<Set<String>, Map<String, List<String>>> dataProviderPair = extractUrisAndLiterals(
+        aggregation.getEdmDataProvider(), organizationPrefLabelMap);
+
+    if (dataProviderPair.getValue().isEmpty()) {
+      return false;
+    } else {
+      if (dataProviderPair.getKey().contains(rdf.getAggregationList().getFirst().getDataProvider().getResource().getResource())) {
+        String dataProvider = dataProviderPair.getValue().values().stream().findFirst().get().getFirst();
+        if (providersList.contains(dataProvider)) {
+          LOGGER.info("Has DataProvider: {} => {}", fullBean.getAbout(), dataProvider);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+
 }
