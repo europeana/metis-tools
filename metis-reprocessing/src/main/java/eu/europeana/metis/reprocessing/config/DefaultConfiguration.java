291587bb80d037b208713b138dd5f814552c6af5
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-03-21 17:23:49 +0100

    MET-6258 udpdate organizations using new client entity api version (#129)
    * MET-6258 udpdate organizations using new client entity api version
    
    * MET-6258 update client entity api and use ClientEntityResolver
    
    * MET-6258 enrichment organizations test
    
    * MET-6258 update pom with newer libraries and related changes
    
    * MET-6258 code review comments

--- metis-reprocessing/src/main/java/eu/europeana/metis/reprocessing/config/DefaultConfiguration.java
+++ metis-reprocessing/src/main/java/eu/europeana/metis/reprocessing/config/DefaultConfiguration.java
@@ -1,14 +1,53 @@
 package eu.europeana.metis.reprocessing.config;
 
+import static eu.europeana.enrichment.api.internal.EntityResolver.europeanaLinkPattern;
+
 import eu.europeana.corelib.solr.bean.impl.FullBeanImpl;
+import eu.europeana.enrichment.api.external.impl.ClientEntityResolver;
+import eu.europeana.enrichment.api.external.model.EnrichmentBase;
+import eu.europeana.enrichment.api.internal.AggregationFieldType;
+import eu.europeana.enrichment.api.internal.EntityResolver;
+import eu.europeana.enrichment.api.internal.ReferenceTermContext;
+import eu.europeana.enrichment.rest.client.dereference.Dereferencer;
+import eu.europeana.enrichment.rest.client.dereference.DereferencerProvider;
+import eu.europeana.enrichment.rest.client.exceptions.DereferenceException;
+import eu.europeana.enrichment.rest.client.exceptions.EnrichmentException;
+import eu.europeana.enrichment.utils.EntityMergeEngine;
+import eu.europeana.entity.client.config.EntityClientConfiguration;
+import eu.europeana.entity.client.web.EntityClientApiImpl;
 import eu.europeana.indexing.exception.IndexingException;
 import eu.europeana.metis.reprocessing.utilities.IndexUtilities;
 import eu.europeana.metis.reprocessing.utilities.PostProcessUtilities;
 import eu.europeana.metis.reprocessing.utilities.ProcessUtilities;
+import eu.europeana.metis.schema.jibx.AboutType;
+import eu.europeana.metis.schema.jibx.Aggregation;
+import eu.europeana.metis.schema.jibx.DataProvider;
+import eu.europeana.metis.schema.jibx.Organization;
+import eu.europeana.metis.schema.jibx.Provider;
 import eu.europeana.metis.schema.jibx.RDF;
+import eu.europeana.metis.schema.jibx.ResourceOrLiteralType;
 import eu.europeana.metis.utils.CustomTruststoreAppender.TrustStoreConfigurationException;
+import eu.europeana.normalization.util.NormalizationConfigurationException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,13 +65,114 @@ public class DefaultConfiguration extends Configuration {
   private final ThrowingTriConsumer<RDF, Boolean, Configuration> rdfIndexer;
   private final ThrowingQuadConsumer<String, Date, Date, Configuration> afterReprocessProcessor;
 
-  public DefaultConfiguration(PropertiesHolderExtension propertiesHolderExtension) throws
-      URISyntaxException, TrustStoreConfigurationException, IndexingException {
+  private ClientEntityResolver entityResolver;
+
+  public DefaultConfiguration(PropertiesHolderExtension propertiesHolderExtension)
+      throws DereferenceException, EnrichmentException, URISyntaxException, TrustStoreConfigurationException, IndexingException, NormalizationConfigurationException {
     super(propertiesHolderExtension);
 
     this.fullBeanProcessor = ProcessUtilities::processFullBean;
     this.rdfIndexer = IndexUtilities::indexRecord;
     this.afterReprocessProcessor = PostProcessUtilities::postProcess;
+
+    initializeAdditionalElements(propertiesHolderExtension);
+  }
+
+  private static void extendEntitiesMap(Map<Class<? extends AboutType>, Set<String>> entities,
+      Class<? extends AboutType> classType, Set<String> entitiesToUpdate) {
+    entities.computeIfAbsent(classType, v -> new HashSet<>());
+    entities.computeIfPresent(classType, (k, v) -> {
+      v.addAll(entitiesToUpdate);
+      return v;
+    });
+  }
+
+  private static <T extends AboutType> Set<String> findMatchingEntityLinks(Set<String> europeanaLinks,
+      Supplier<List<T>> entitySupplier) {
+    return Optional.ofNullable(entitySupplier.get()).orElseGet(Collections::emptyList)
+                   .stream().map(AboutType::getAbout)
+                   .filter(europeanaLinks::contains)
+                   .collect(Collectors.toSet());
+  }
+
+  private static void replaceEntities(RDF rdf,
+      HashMap<Class<? extends AboutType>, Map<ReferenceTermContext, List<EnrichmentBase>>> enrichedEntities) {
+    for (Map.Entry<Class<? extends AboutType>, Map<ReferenceTermContext, List<EnrichmentBase>>> entry : enrichedEntities.entrySet()) {
+      Map<ReferenceTermContext, List<EnrichmentBase>> dereferencedEntities = entry.getValue();
+
+      List<? extends AboutType> entitiesList = new ArrayList<>();
+      if (entry.getKey().isNestmateOf(Organization.class)) {
+        entitiesList = rdf.getOrganizationList();
+      }
+
+      findAndReplaceUpdatedEntity(rdf, dereferencedEntities, entitiesList);
+    }
+  }
+
+  private static void findAndReplaceUpdatedEntity(RDF rdf, Map<ReferenceTermContext, List<EnrichmentBase>> referenceTermListMap,
+      List<? extends AboutType> entitiesList) {
+    //At this point the dereference values should contain 0 or 1 but not more results per reference
+    for (Map.Entry<ReferenceTermContext, List<EnrichmentBase>> referenceTermListEntry : referenceTermListMap.entrySet()) {
+      if (CollectionUtils.isNotEmpty(referenceTermListEntry.getValue())
+          && referenceTermListEntry.getValue().getFirst() != null) {
+        EnrichmentBase enrichmentBase = referenceTermListEntry.getValue().getFirst();
+
+        //Find if we have a match and update current rdf
+        ListIterator<? extends AboutType> entitiesListIterator = entitiesList.listIterator();
+        boolean removedOld = false;
+        while (entitiesListIterator.hasNext()) {
+          String about = entitiesListIterator.next().getAbout();
+          if (referenceTermListEntry.getKey().getReference().toString().equals(about)) {
+            entitiesListIterator.remove();
+            removedOld = true;
+            updateAggregationResources(rdf, about, enrichmentBase);
+            break;
+          }
+        }
+
+        //Replace if removed
+        if (removedOld) {
+          EntityMergeEngine.convertAndAddEntity(rdf, enrichmentBase);
+        }
+      }
+    }
+  }
+
+  private static void updateAggregationResources(RDF rdf, String about, EnrichmentBase enrichmentBase) {
+    List<Aggregation> list = new ArrayList<>();
+    for (Aggregation aggregation : rdf.getAggregationList()) {
+      if (aggregation.getDataProvider().getResource() != null
+          && aggregation.getDataProvider().getResource().getResource()
+                        .equals(about)) {
+        DataProvider dataProvider = aggregation.getDataProvider();
+        dataProvider.getResource().setResource(enrichmentBase.getAbout());
+      }
+      if (aggregation.getProvider().getResource() != null
+          && aggregation.getProvider().getResource().getResource()
+                        .equals(about)) {
+        Provider provider = aggregation.getProvider();
+        provider.getResource().setResource(enrichmentBase.getAbout());
+      }
+      if (aggregation.getIntermediateProviderList() != null) {
+        aggregation.getIntermediateProviderList().forEach(p -> {
+              if (p.getResource().getResource().equals(about)) {
+                p.getResource().setResource(enrichmentBase.getAbout());
+              }
+            }
+        );
+      }
+
+      list.add(aggregation);
+    }
+    rdf.setAggregationList(list);
+  }
+
+  private static URL getUrl(String link) {
+    try {
+      return URI.create(link).toURL();
+    } catch (MalformedURLException e) {
+      return null;
+    }
   }
 
   @Override
@@ -52,8 +192,80 @@ public class DefaultConfiguration extends Configuration {
 
   @Override
   public RDF processRDF(RDF rdf) {
-    //Modify this method accordingly
+
+    rdf = updateOrganizations(rdf);
     LOGGER.debug("DONE");
+    return rdf;
+  }
+
+  private void initializeAdditionalElements(PropertiesHolderExtension propertiesHolderExtension) {
+    entityResolver = (ClientEntityResolver) prepareClientEntityResolver(propertiesHolderExtension);
+  }
+
+  private EntityResolver prepareClientEntityResolver(PropertiesHolderExtension propertiesHolderExtension) {
+    //Sanity check
+    if (StringUtils.isAnyBlank(propertiesHolderExtension.entityManagementUrl, propertiesHolderExtension.entityApiUrl,
+        propertiesHolderExtension.entityApiKey)) {
+      throw new IllegalArgumentException("Requested resolver but configuration is missing");
+    }
+    final Properties properties = new Properties();
+    properties.put("entity.management.url", propertiesHolderExtension.entityManagementUrl);
+    properties.put("entity.api.url", propertiesHolderExtension.entityApiUrl);
+    properties.put("entity.api.key", propertiesHolderExtension.entityApiKey);
+
+    return new ClientEntityResolver(new EntityClientApiImpl(new EntityClientConfiguration(properties)),
+        propertiesHolderExtension.enrichmentBatchSize);
+  }
+
+  private Dereferencer getDereferencer(PropertiesHolderExtension propertiesHolderExtension) throws DereferenceException {
+    if (StringUtils.isNotBlank(propertiesHolderExtension.dereferenceUrl)) {
+      final DereferencerProvider dereferencerProvider = new DereferencerProvider();
+      dereferencerProvider.setEnrichmentPropertiesValues(propertiesHolderExtension.entityManagementUrl,
+          propertiesHolderExtension.entityApiUrl, propertiesHolderExtension.entityApiKey);
+      dereferencerProvider.setDereferenceUrl(propertiesHolderExtension.dereferenceUrl);
+      return dereferencerProvider.create();
+    }
+    return null;
+  }
+
+  private HashMap<Class<? extends AboutType>, Map<ReferenceTermContext, List<EnrichmentBase>>> enrichEntities(
+      Map<Class<? extends AboutType>, Set<String>> entitiesLinksToDereference) {
+    HashMap<Class<? extends AboutType>, Map<ReferenceTermContext, List<EnrichmentBase>>> enrichedResultEntities = new HashMap<>();
+
+    for (Map.Entry<Class<? extends AboutType>, Set<String>> entry : entitiesLinksToDereference.entrySet()) {
+      Set<ReferenceTermContext> referenceTerms = entry.getValue().stream()
+                                                      .map(DefaultConfiguration::getUrl).filter(Objects::nonNull)
+                                                      .map(url -> new ReferenceTermContext(url, new HashSet<>()))
+                                                      .collect(Collectors.toSet());
+      Map<ReferenceTermContext, List<EnrichmentBase>> enrichedReferences = entityResolver.resolveByUri(referenceTerms);
+      enrichedResultEntities.put(entry.getKey(), enrichedReferences);
+    }
+
+    return enrichedResultEntities;
+  }
+
+  RDF updateOrganizations(RDF rdf) {
+    LOGGER.info("Organization Update");
+    //Find europeana id organizations that are linked in provider aggregation supported fields
+    List<Aggregation> aggregationList = rdf.getAggregationList();
+    Set<String> aggregationEuropeanaLinks = new HashSet<>();
+    for (AggregationFieldType aggregationFieldType : AggregationFieldType.values()) {
+      aggregationList.stream().flatMap(aggregationFieldType::extractFields)
+                     .map(ResourceOrLiteralType::getResource)
+                     .filter(Objects::nonNull)
+                     .map(ResourceOrLiteralType.Resource::getResource)
+                     .filter(europeanaLinkPattern.asPredicate()) // check if only this organization
+                     .forEach(aggregationEuropeanaLinks::add);
+    }
+    //Check all present entities and collect links that match
+    final Map<Class<? extends AboutType>, Set<String>> entitiesToUpdate = new HashMap<>();
+    extendEntitiesMap(entitiesToUpdate, Organization.class,
+        findMatchingEntityLinks(aggregationEuropeanaLinks, rdf::getOrganizationList));
+
+    //Request new entities
+    HashMap<Class<? extends AboutType>, Map<ReferenceTermContext, List<EnrichmentBase>>> enrichedEntities = enrichEntities(
+        entitiesToUpdate);
+    replaceEntities(rdf, enrichedEntities);
     return rdf;
   }
 
