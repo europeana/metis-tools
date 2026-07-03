73772f1ca204bdd71ff8dc9caba94055c7eb4ec0
jochen_vermeulen <jochen.vermeulen@europeana.eu>
2026-07-03 15:35:25 +0200

    Harvest Published Records: Step 2, get and write record content.

--- metis-published-records-harvester/src/main/java/eu/europeana/metis/published/records/harvest/Main.java
+++ metis-published-records-harvester/src/main/java/eu/europeana/metis/published/records/harvest/Main.java
@@ -1,6 +1,13 @@
 package eu.europeana.metis.published.records.harvest;
 
+import eu.europeana.cloud.client.uis.rest.CloudException;
+import eu.europeana.cloud.client.uis.rest.UISClient;
+import eu.europeana.cloud.common.model.File;
+import eu.europeana.cloud.common.model.Representation;
 import eu.europeana.cloud.common.model.Revision;
+import eu.europeana.cloud.mcs.driver.FileServiceClient;
+import eu.europeana.cloud.mcs.driver.RecordServiceClient;
+import eu.europeana.cloud.service.uis.exception.RecordDoesNotExistException;
 import eu.europeana.metis.core.common.DaoFieldNames;
 import eu.europeana.metis.core.dao.WorkflowExecutionDao;
 import eu.europeana.metis.core.dao.WorkflowExecutionDao.ResultList;
@@ -8,18 +15,25 @@ import eu.europeana.metis.core.workflow.WorkflowExecution;
 import eu.europeana.metis.core.workflow.WorkflowStatus;
 import eu.europeana.metis.core.workflow.plugins.AbstractMetisPlugin;
 import eu.europeana.metis.core.workflow.plugins.IndexToPublishPlugin;
+import eu.europeana.metis.core.workflow.plugins.MetisPlugin;
 import eu.europeana.metis.core.workflow.plugins.PluginType;
 import eu.europeana.metis.published.records.harvest.utils.Application;
-import eu.europeana.metis.utils.CustomTruststoreAppender.TrustStoreConfigurationException;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,67 +41,16 @@ public class Main {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-  // TODO TEMP
-  private static final String INPUT = """
-      /1227/europeana_fashion_0
-      /1227/europeana_fashion_1
-      /1227/europeana_fashion_10
-      /1227/europeana_fashion_11
-      /1227/europeana_fashion_12
-      /1227/europeana_fashion_13
-      /1227/europeana_fashion_14
-      /1227/europeana_fashion_15
-      /1227/europeana_fashion_16
-      /1227/europeana_fashion_2
-      /1227/europeana_fashion_3
-      /1227/europeana_fashion_4
-      /1227/europeana_fashion_5
-      /1227/europeana_fashion_6
-      /1227/europeana_fashion_7
-      /1227/europeana_fashion_8
-      /1227/europeana_fashion_9
-      /1517/urn___en_home_1_261e176f_9890_4136_8edf_c554bd4b7ee7
-      /1517/urn___en_home_1_2dcba0a1_d64b_4a15_854a_6e513f8ca345
-      /1517/urn___en_home_1_d4149df4_ae68_4468_83cf_a8518ead9c0c
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_1
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_10
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_11
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_12
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_13
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_14
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_15
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_2
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_3
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_4
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_5
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_6
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_7
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_8
-      /654/_providedCHO_https___hdl_handle_net_10622_COLL00415_https___hdl_handle_net_10622_COLL00415_9
-      /9200309/BibliographicResource_3000093756777_source
-      /9200309/BibliographicResource_3000093756781_source
-      /9200309/BibliographicResource_3000093756782_source
-      /9200309/BibliographicResource_3000093756772_source
-      /9200309/BibliographicResource_3000093756774_source
-      /9200309/BibliographicResource_3000093756780_source
-      /9200309/BibliographicResource_3000093756784_source
-      /9200309/BibliographicResource_3000093756776_source
-      /9200309/BibliographicResource_3000093756778_source
-      /9200309/BibliographicResource_3000093756775_source
-      /9200309/BibliographicResource_3000093756779_source
-      /9200309/BibliographicResource_3000093756773_source
-      /9200309/BibliographicResource_3000093756785_source
-      /9200309/BibliographicResource_3000093756787_source
-      /111/https___hispana_mcu_es_lod_oai_jable_ulpgc_es_pandora_0006068939_ent0
-      """;
+  private static final String INPUT_FILE = "/home/jochen/Desktop/published_record_harvest/sample_collection_test_upgrade.txt";
 
-  public static void main(String[] args) throws TrustStoreConfigurationException, IOException {
+  private static final String DESTINATION_DIRECTORY = "/home/jochen/Desktop/published_record_harvest/records";
+
+  public static void main(String[] args) throws Exception {
     try (final Application application = Application.initialize()) {
       final Map<String, Set<String>> records = readInput();
       final Map<String, List<Revision>> revisions = getLatestPublishRevisions(records.keySet(),
           application);
-      System.out.println(revisions.size());
+      downloadRecords(records, revisions, application);
     }
   }
 
@@ -97,8 +60,10 @@ public class Main {
    */
   private static Map<String, Set<String>> readInput() throws IOException {
     final Map<String, Set<String>> result = new HashMap<>();
-    try (final StringReader input = new StringReader(INPUT); // TODO Make FileReader
-        final BufferedReader reader = new BufferedReader(input)) {
+    final AtomicInteger counter = new AtomicInteger();
+    LOGGER.info("Reading record IDs ...");
+    try (final InputStream input = Files.newInputStream(Path.of(INPUT_FILE));
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
       while (true) {
         final String line = reader.readLine();
         if (line == null) {
@@ -115,8 +80,12 @@ public class Main {
         }
         result.computeIfAbsent(trimmedLine.substring(1, datasetEnd), key -> new HashSet<>())
             .add(trimmedLine);
+        if (counter.incrementAndGet() % 1000 == 0) {
+          LOGGER.info("  {} record IDs read.", counter.get());
+        }
       }
     }
+    LOGGER.info("Total of {} valid record IDs found.", counter.get());
     return result;
   }
 
@@ -131,9 +100,15 @@ public class Main {
     final Map<String, List<Revision>> result = new HashMap<>();
     final WorkflowExecutionDao workflowExecutionDao = new WorkflowExecutionDao(
         application.getDatastoreProvider());
+    final AtomicInteger counter = new AtomicInteger();
+    LOGGER.info("Reading revisions for datasets ...");
     for (final String dataset : datasets) {
       result.put(dataset, getLatestPublishRevisions(dataset, workflowExecutionDao, application));
+      if (counter.incrementAndGet() % 100 == 0) {
+        LOGGER.info("  {} datasets done.", counter.get());
+      }
     }
+    LOGGER.info("Revisions read for a total of {} datasets.", counter.get());
     return result;
   }
 
@@ -165,5 +140,135 @@ public class Main {
     }
     LOGGER.error("No (full processing) plugin is found for dataset {}.", dataset);
     return result;
+  }
+
+  /**
+   * Download all records and save them to the right place.
+   *
+   * @param records     The records sorted by dataset.
+   * @param revisions   The revisions listed in reverse chronological order, per dataset.
+   * @param application This application.
+   * @throws Exception For issues.
+   */
+  private static void downloadRecords(Map<String, Set<String>> records,
+      Map<String, List<Revision>> revisions, Application application) throws Exception {
+    final AtomicInteger counter = new AtomicInteger();
+    LOGGER.info("Writing record contents ...");
+    try (final UISClient uisClient = new UISClient(application.getProperties().ecloudMcsBaseUrl,
+        application.getProperties().ecloudUsername, application.getProperties().ecloudPassword,
+        10_000, 10_000);
+        final RecordServiceClient recordServiceClient = new RecordServiceClient(
+            application.getProperties().ecloudMcsBaseUrl,
+            application.getProperties().ecloudUsername, application.getProperties().ecloudPassword,
+            10_000, 10_000);
+        final FileServiceClient fileServiceClient = new FileServiceClient(
+            application.getProperties().ecloudMcsBaseUrl,
+            application.getProperties().ecloudUsername, application.getProperties().ecloudPassword,
+            10_000, 10_000)) {
+      for (Map.Entry<String, Set<String>> datasetEntry : records.entrySet()) {
+        final String dataset = datasetEntry.getKey();
+        for (String record : datasetEntry.getValue()) {
+          final String recordContents = getRecordFromECloud(record, revisions.get(dataset),
+              application, uisClient, recordServiceClient, fileServiceClient);
+          if (recordContents != null) {
+            saveRecordToFile(dataset, record, recordContents);
+          }
+          if (counter.incrementAndGet() % 10 == 0) {
+            LOGGER.info("  {} records done.", counter.get());
+          }
+        }
+      }
+      LOGGER.info("Total of {} records written.", counter.get());
+    }
+  }
+
+  /**
+   * @param record              The record ID
+   * @param datasetRevisions    The known revisions, in reverse order.
+   * @param application         This application.
+   * @param uisClient           eCloud client.
+   * @param recordServiceClient eCloud client.
+   * @param fileServiceClient   eCloud client.
+   * @return The record contents, or null if record could not be found.
+   * @throws Exception For issues.
+   */
+  private static String getRecordFromECloud(String record, List<Revision> datasetRevisions,
+      Application application, UISClient uisClient, RecordServiceClient recordServiceClient,
+      FileServiceClient fileServiceClient) throws Exception {
+
+    // Get the eCloud ID.
+    final String ecloudId;
+    try {
+      ecloudId = uisClient.getCloudId(application.getProperties().ecloudProvider,
+          record).getId();
+    } catch (CloudException e) {
+      if (e.getCause() instanceof RecordDoesNotExistException) {
+        LOGGER.error("Could not find record ID in eCloud: {}.", record);
+        return null;
+      } else {
+        throw e;
+      }
+    }
+
+    // Get the record contents.
+    for (Revision revision : datasetRevisions) {
+      // Check all revisions: most recent one with the record is the one we need.
+      final String recordContents = getRecordByEcloudIdAndRevision(revision, ecloudId,
+          recordServiceClient, fileServiceClient);
+      if (recordContents != null) {
+        return recordContents;
+      }
+    }
+    LOGGER.error("Could not find record in eCloud: {}.", record);
+    return null;
+  }
+
+  /**
+   * @param revision            The revision of the record.
+   * @param ecloudId            The eCloud ID of the record.
+   * @param recordServiceClient A client object for eCloud.
+   * @param fileServiceClient   A client object for eCloud.
+   * @return The record contents, or null if record does not exist for this revision.
+   * @throws Exception For issues.
+   */
+  private static String getRecordByEcloudIdAndRevision(Revision revision, String ecloudId,
+      RecordServiceClient recordServiceClient, FileServiceClient fileServiceClient)
+      throws Exception {
+
+    // Get the representation(s) for the given combination of plugin and record ID.
+    final List<Representation> representations = recordServiceClient
+        .getRepresentationsByRevision(ecloudId, MetisPlugin.getRepresentationName(), revision);
+    if (representations == null || representations.isEmpty()) {
+      return null;
+    }
+    final Representation representation = representations.getFirst();
+
+    // Perform checks on the file lists.
+    if (representation.getFiles() == null || representation.getFiles().isEmpty()) {
+      return null;
+    }
+    final File file = representation.getFiles().getFirst();
+
+    // Obtain the file contents belonging to this representation version.
+    final InputStream inputStream = fileServiceClient.getFile(file.getContentUri().toString());
+    return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+  }
+
+  /**
+   * Saves the record to the destination directory.
+   *
+   * @param dataset        The dataset ID.
+   * @param recordId       The record ID.
+   * @param recordContents The record contents.
+   */
+  private static void saveRecordToFile(String dataset, String recordId, String recordContents)
+      throws IOException {
+    final Path directory = Path.of(DESTINATION_DIRECTORY, dataset);
+    if (!Files.exists(directory)) {
+      Files.createDirectory(directory);
+    }
+    final Path file = directory.resolve(recordId.substring(recordId.lastIndexOf('/') + 1) + ".xml");
+    Files.writeString(file, recordContents, StandardOpenOption.CREATE,
+        StandardOpenOption.TRUNCATE_EXISTING);
   }
 }
